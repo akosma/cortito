b13fb7c946a6949f4ee983f1b8bcfab63289a296
Adrian Kosmaczewski <akosma@gmail.com>
2009-06-05 12:04:55 +0200

    Refactored the code for readability

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,55 +18,66 @@ class ItemsController < ApplicationController
 
     if !params.has_key?(:url)
       render :template => "items/new"
-    else
-      url = params[:url]
-      short = nil
-      
-      if params.has_key?(:short)
-        short = CGI::escape(params[:short])
-      end
-      
-      if url.length == 0
-        render :template => "items/invalid"
-      else
-        shortened_url_prefix = ["http://tinyurl.com/", "http://url.akosma.com/",
-          "http://u.nu/", "http://snipurl.com/", "http://readthisurl.com/",
-          "http://doiop.com/", "http://urltea.com/", "http://dwarfurl.com/", 
-          "http://memurl.com/", "http://shorl.com/", "http://traceurl.com/", 
-          "http://bit.ly/"]
-        
-        shortened_url_prefix.each do |prefix|
-          if url.starts_with?(prefix)
-            render :template => "items/invalid"
-            return
-          end
-        end
-
-        if url.length < ("http://".length + @host.length + 1 + Item::SHORT_URL_LENGTH)
-          render :template => "items/short"
-        else
-        
-          @item = Item.find_by_original(url)
-          if not @item
-            @item = Item.new
-            @item.original = params[:url]
-            @item.shortened = short
-            @item.save
-          end
-      
-          respond_to do |format|
-            format.html do
-              @short_url = ["http://", @host, "/", @item.shortened].join
-              @twitter_url = ["http://twitter.com/home?status=", @short_url].join
-              render :template => "items/show"
-            end
-            format.xml { render :text => ["http://", @host, "/", @item.shortened].join }
-            format.js { render :text => ["http://", @host, "/", @item.shortened].join }
-          end
-        end
-      end
-
+      return
     end
+
+    url = params[:url]
+    short = nil
+    
+    if params.has_key?(:short)
+      short = CGI::escape(params[:short])
+    end
+    
+    if url.length == 0
+      render :template => "items/invalid"
+      return
+    end
+
+    if is_already_shortened_url?(url)
+      render :template => "items/invalid"
+      return
+    end
+
+    if url.length < ("http://".length + @host.length + 1 + Item::SHORT_URL_LENGTH)
+      render :template => "items/short"
+      return
+    end
+  
+    @item = Item.find_by_original(url)
+    if not @item
+      @item = Item.new
+      @item.original = params[:url]
+      @item.shortened = short
+      @item.save
+    end
+
+    respond_to do |format|
+      format.html do
+        @short_url = ["http://", @host, "/", @item.shortened].join
+        @twitter_url = ["http://twitter.com/home?status=", @short_url].join
+        render :template => "items/show"
+      end
+      format.xml { render :text => ["http://", @host, "/", @item.shortened].join }
+      format.js { render :text => ["http://", @host, "/", @item.shortened].join }
+    end
+
+  end
+
+private
+
+  def is_already_shortened_url?(url)
+    shortened_url_prefix = ["http://tinyurl.com/", "http://url.akosma.com/",
+      "http://u.nu/", "http://snipurl.com/", "http://readthisurl.com/",
+      "http://doiop.com/", "http://urltea.com/", "http://dwarfurl.com/", 
+      "http://memurl.com/", "http://shorl.com/", "http://traceurl.com/", 
+      "http://bit.ly/"]
+    
+    shortened_url_prefix.each do |prefix|
+      if url.starts_with?(prefix)
+        return true
+      end
+    end
+    return false
   end
 
 end
