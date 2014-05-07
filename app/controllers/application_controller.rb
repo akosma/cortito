e67cb972f185956866fa21ebb6017e53f159f38a
Adrian Kosmaczewski <ako@trifork.com>
2014-05-07 10:14:47 +0200

    Removing files, preparing for migration to PHP

--- app/controllers/application_controller.rb
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2009, Adrian Kosmaczewski / akosma software
-# All rights reserved.
-# BSD License. See LICENSE.txt for details.
-
-class ApplicationController < ActionController::Base
-  helper :all
-end
